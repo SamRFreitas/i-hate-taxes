44230400e7cb775260955c7b5b68d9ac7b3af393
samrfreitas <samuelrfreitassi@gmail.com>
2022-06-19 22:52:08 -0300

    Add class DAS in the project and test

---
+++ src/models/Das/Das.ts
@@ -0,0 +1,18 @@
+import { calculatePercentageValue } from "@/utils/calculate"
+import { formatAmount } from "@/utils/format"
+
+export class Das {
+  amount: number = 0  
+  dasPercentage: number = 3.05
+  dasTax: number
+
+  constructor (amount: number) {
+    this.amount = amount
+    this.dasTax = this.getDasTax()
+  }
+
+  getDasTax () {
+    let percentageValue = calculatePercentageValue(this.amount, this.dasPercentage)
+    return formatAmount(percentageValue, 2)
+  }
+}
