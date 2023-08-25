174c8f2b62199ee0efa7b7b085b28c34bcc7615b
samrfreitas <samuelrfreitassi@gmail.com>
2023-08-25 16:05:32 -0300

    test: updating test due to the new feature to check if is national

--- src/models/Das/das.spec.ts
+++ src/models/Das/das.spec.ts
@@ -2,10 +2,19 @@ import { Das } from './Das'
 
 describe('Das test - ', () => {
 
-  it('getDasTax', () => {
-    const das = new Das(100)
+  describe('getDasTax', () => {
+    it('When isNational, dasTax should be 3.05', () => {
+      const das = new Das(100)
+      
+      expect(das.dasTax).toBe(3.05)
+    })
+
+    it('When NOT isNational, dasTax should be 6.5', () => {
+      const das = new Das(100, true)
+      
+      expect(das.dasTax).toBe(6.5)
+    })
     
-    expect(das.dasTax).toBe(6.5)
   })
   
 })
