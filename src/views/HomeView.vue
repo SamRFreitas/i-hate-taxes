0b6f96a7835d36a8e993d06c5b53052b828070be
samrfreitas <samuelrfreitassi@gmail.com>
2023-08-30 20:56:04 -0300

    feature: new main screen version implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="flex justify-center items-center">
-    <img alt="Vue logo" src="../assets/purple-devil.png" width="200" height="200">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="flex flex-col justify-center items-center ">
+    <FormView @update-taxes="updatecalculatedValues($event)"/>
+    <TableData :values="state.calculatedValues" class="mt-4"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+import { defineComponent, reactive } from 'vue'
+import FormView from './FormView.vue'
+import TableData from '../components/TableData.vue'
+import { Vrau } from '@/models/Vrau/Vrau'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    FormView,
+    TableData
   },
-});
+  setup () {
+
+    const state = reactive({
+
+      calculatedValues: {
+        contabilizeiMonthlyFee: 0,
+        inss: 0,
+        irrf: 0,
+        das: 0,
+        proLabore: 0,
+        dla: 0,
+        taxesTotal: 0,
+        profit: 0,
+      }
+
+    })
+
+
+    function updatecalculatedValues(vrau: Vrau): void {
+
+      state.calculatedValues.contabilizeiMonthlyFee  = vrau.contabilizeiMonthlyFee
+      state.calculatedValues.inss = vrau.inss.getInssTax()
+      state.calculatedValues.irrf = vrau.inss.getIrrfTax()
+      state.calculatedValues.das = vrau.das.dasTax
+      state.calculatedValues.dla = vrau.dla
+      state.calculatedValues.proLabore = vrau.inss.getProLabore()
+      state.calculatedValues.taxesTotal = vrau.inss.getInssTax() + vrau.inss.getIrrfTax() + vrau.das.dasTax + vrau.contabilizeiMonthlyFee
+      state.calculatedValues.profit = vrau.inss.getProLabore() + vrau.dla
+      
+    }
+
+    return {
+      state,
+      updatecalculatedValues
+    }
+  }
+})
 </script>
