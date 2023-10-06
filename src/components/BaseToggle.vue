b84b029eb53a7b46c35fef9bf01847fb257d2214
samrfreitas <samuelrfreitassi@gmail.com>
2023-10-06 10:41:28 -0300

    style: change toggle size

--- src/components/BaseToggle.vue
+++ src/components/BaseToggle.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="modelValue ? 'bg-primary' : 'bg-gray-500'" @click="toggle" class="w-14 transition-all duration-500 h-6 flex items-center rounded-full p-1 cursor-pointer">
-    <div :class="{'translate-x-6': modelValue}" class="w-6 h-6 transition-all duration-500 transform bg-white rounded-full shadow-md"></div>
+    <div :class="{'translate-x-6': modelValue}" class="w-5 h-5 transition-all duration-500 transform bg-white rounded-full shadow-md"></div>
   </div>
 </template>
 
